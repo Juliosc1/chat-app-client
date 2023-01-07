bc5ac4c7c361c36256b4fbcf1cfad97cd4d2ddd0
juliosc1 <julio.siklander@gmail.com>
2023-01-07 20:08:45 +0100

    add chat box

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,6 +4,7 @@ import { over } from "stompjs";
 
 let stompClient = null;
 const Chat = () => {
+  const [chatMessage, setChatMessage] = useState([]);
   const [userData, setUserData] = useState({
     username: "",
     connected: false,
@@ -20,11 +21,63 @@ const Chat = () => {
   const connectUser = () => {
     let socket = new SockJS("http://localhost:8080/ws") 
     stompClient = over(socket);
-    ;
+    stompClient.connect({}, onConnected);
   }
+
+  const onConnected = () => {
+    setUserData({...userData, connected: true});
+    stompClient.subscribe("/chatroom/public", onMessageReceived)
+  }
+
+  const onMessageReceived = () => {
+    let payloadData = JSON.parse(payloadData);
+    switch (payloadData.status) {
+      case "CONNECT":
+        break;
+      case "MESSAGE":
+        chatMessage.push(payloadData);
+        setChatMessage([...chatMessage]);
+        break;
+    }
+  }
+
+  const userJoin = () => {
+    let message = {
+      senderName: userData.username,
+      status: "CONNECT"
+    };
+    stompClient.send("/app/message", {}, JSON.stringify(message));
+  };
 
   return (
     <div>
+      {userData.connected ? (
+        <div>
+          <p>Chatroom</p>
+          {(
+            <div>
+              <ul>{chatMessage.map((chat, index) => (
+                <li>{chat.senderName !== userData.username && (
+                  <div>{chat.senderName}</div>
+                  )}
+                  <div>{chat.message}</div>
+                  {chat.senderName === userData.username && (
+                    <div>{chat.senderName}</div>
+                  )}
+                </li>
+                ))}
+              </ul>
+
+              
+              <div>
+                <input type="text" placeholder="Enter the message" value={userData.message}/>
+                <button>Send Message</button>
+              </div>
+
+            </div>
+          )}
+        </div>
+      ) : (
       <div>
         <h1>Register</h1>
         <input
@@ -37,6 +90,8 @@ const Chat = () => {
         />
         <button onClick={connectUser} type="button" className="bg-blue-500">Connect</button>
       </div>
+
+      )}
     </div>
   );
 };
