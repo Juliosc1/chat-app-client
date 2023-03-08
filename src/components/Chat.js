2f6b294b2933a539fc05fddee9a365be5a1a0689
juliosc1 <julio.siklander@gmail.com>
2023-03-08 16:01:53 +0100

    refactor

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -17,7 +17,7 @@ const Chat = () => {
 
   //register input
   const handleUsername = (e) => {
-    const {value} = e.target;
+    const { value } = e.target;
     setUserData({ ...userData, username: value });
     console.log(userData);
   };
@@ -41,7 +41,7 @@ const Chat = () => {
     };
     stompClient.send("/app/message", {}, JSON.stringify(message));
   };
-  
+
   const onMessageReceived = (payload) => {
     let payloadData = JSON.parse(payload.body);
     switch (payloadData.status) {
@@ -55,9 +55,9 @@ const Chat = () => {
   };
 
   const handleMessage = (e) => {
-    const {value} = e.target;
-    setUserData({...userData, message: value});
-  }
+    const { value } = e.target;
+    setUserData({ ...userData, message: value });
+  };
 
   const SendMessage = () => {
     if (stompClient) {
@@ -66,77 +66,100 @@ const Chat = () => {
         message: userData.message,
         status: "MESSAGE",
       };
-      console.log(messanger)
+      console.log(messanger);
       stompClient.send("/app/message", {}, JSON.stringify(messanger));
-      setUserData({...userData, message: ""})
+      setUserData({ ...userData, message: "" });
     }
-  }
+  };
 
   return (
     <div className="bg-neutral-900 flex flex-col justify-center items-center h-[100vh] w-full p-10">
-      
-        {userData.connected ? (
-          <div className="bg-neutral-800 flex flex-col items-center h-screen w-2/3 rounded-2xl border-2">
-            <div className="flex justify-between items-center w-[90%] border-b-2 mb-5">
-              <p className="text-4xl text-white font-bold my-5 w-full">Chatroom</p>
-              <p className="bg-blue-500 w-20 h-10 flex items-center justify-center text-xl text-white border-2 rounded-full mt-2">{userData.username}</p>
-            </div>
-            {
-              <div className="flex flex-col justify-between h-full w-[90%]">
-                <ul className="max-h-[70vh] w-full overflow-auto">
-                  {chatMessage.map((chat, index) => (
-                    <li className={`flex justify-end items-center h-14 gap-8 ${chat.senderName === userData.username && "self"}`} key={index}>
-                      
-                      {chat.senderName !== userData.username && (
-                      <div className="bg-green-400 text-white flex items-center px-4 py-1 rounded-full ">{chat.message}</div>
-                      )}
+      {userData.connected ? (
+        <div className="bg-neutral-800 flex flex-col items-center h-screen w-2/3 rounded-2xl border-2">
+          <div className="flex justify-between items-center w-[90%] border-b-2 mb-5">
+            <p className="text-4xl text-white font-bold my-5 w-full">
+              Chatroom
+            </p>
+            <p className="bg-blue-500 w-20 h-10 flex items-center justify-center text-xl text-white border-2 rounded-full mt-2">
+              {userData.username}
+            </p>
+          </div>
+          {
+            <div className="flex flex-col justify-between h-full w-[90%]">
+              <ul className="max-h-[70vh] w-full overflow-auto">
+                {chatMessage.map((chat, index) => (
+                  <li
+                    className={`flex justify-end items-center h-14 gap-8 ${
+                      chat.senderName === userData.username && "self"
+                    }`}
+                    key={index}
+                  >
+                    {chat.senderName !== userData.username && (
+                      <div className="bg-green-400 text-white flex items-center px-4 py-1 rounded-full ">
+                        {chat.message}
+                      </div>
+                    )}
 
-                      {chat.senderName === userData.username && (
-                      <div className="bg-emerald-600 text-white flex items-center px-4 py-1 rounded-full ">{chat.message}</div>
-                      )}
+                    {chat.senderName === userData.username && (
+                      <div className="bg-emerald-600 text-white flex items-center px-4 py-1 rounded-full ">
+                        {chat.message}
+                      </div>
+                    )}
 
-                      {chat.senderName !== userData.username && (
-                        <div className="border-2 bg-green-500 flex justify-end items-center rounded-full text-white h-10 w-auto p-1">{chat.senderName}</div>
-                      )}
-                      
-                      {chat.senderName === userData.username && (
-                        <div className="border-2 bg-emerald-600 flex justify-end items-center rounded-full text-white h-10 w-auto p-1">{chat.senderName}</div>
-                      )}
-                    </li>
-                  ))}
-                </ul>
+                    {chat.senderName !== userData.username && (
+                      <div className="border-2 bg-green-500 flex justify-end items-center rounded-full text-white h-10 w-auto p-1">
+                        {chat.senderName}
+                      </div>
+                    )}
 
-                <div className="bg-neutral-900 flex justify-between w-full p-2 gap-2 rounded-2xl px-5 mb-5">
-                  <input
-                    type="text"
-                    placeholder="Enter the message"
-                    value={userData.message}
-                    onChange={handleMessage}
-                    className="bg-neutral-900 h-full w-5/6 rounded-full px-2 text-white outline-0"
-                  />
-                  <button type="button" onClick={SendMessage} className="bg-green-500 h-10 w-16 border-2 rounded-full text-white">Send</button>
-                </div>
+                    {chat.senderName === userData.username && (
+                      <div className="border-2 bg-emerald-600 flex justify-end items-center rounded-full text-white h-10 w-auto p-1">
+                        {chat.senderName}
+                      </div>
+                    )}
+                  </li>
+                ))}
+              </ul>
+
+              <div className="bg-neutral-900 flex justify-between w-full p-2 gap-2 rounded-2xl px-5 mb-5">
+                <input
+                  type="text"
+                  placeholder="Enter the message"
+                  value={userData.message}
+                  onChange={handleMessage}
+                  className="bg-neutral-900 h-full w-5/6 rounded-full px-2 text-white outline-0"
+                />
+                <button
+                  type="button"
+                  onClick={SendMessage}
+                  className="bg-green-500 h-10 w-16 border-2 rounded-full text-white"
+                >
+                  Send
+                </button>
               </div>
-            }
-          </div>
-        ) : (
-          <div className="bg-neutral-800 h-96 w-96 flex flex-col justify-center items-center gap-4 rounded-xl">
-            <h1 className="text-white font-bold text-4xl">Register</h1>
-            <input
-              type="text"
-              name="userName"
-              id="user-name"
-              placeholder="Enter your name"
-              onChange={handleUsername}
-              className="h-10 rounded-xl px-2 outline-0"
-            />
-            <button onClick={connectUser} type="button" className="bg-green-600 h-10 w-28 rounded-xl text-white font-bold text-xl">
-              Connect
-            </button>
-          </div>
-        )}
-
-      
+            </div>
+          }
+        </div>
+      ) : (
+        <div className="bg-neutral-800 h-96 w-96 flex flex-col justify-center items-center gap-4 rounded-xl">
+          <h1 className="text-white font-bold text-4xl">Register</h1>
+          <input
+            type="text"
+            name="userName"
+            id="user-name"
+            placeholder="Enter your name"
+            onChange={handleUsername}
+            className="h-10 rounded-xl px-2 outline-0"
+          />
+          <button
+            onClick={connectUser}
+            type="button"
+            className="bg-green-600 h-10 w-28 rounded-xl text-white font-bold text-xl"
+          >
+            Connect
+          </button>
+        </div>
+      )}
     </div>
   );
 };
